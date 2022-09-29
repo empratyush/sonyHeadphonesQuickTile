eccfd8dfcca964eefedfa506959d3b3f3a449bb3
Pratyush <codelab@pratyush.dev>
2022-09-29 18:31:30 +0530

    handle bluetooth permission on configuration activity

--- app/src/main/java/dev/pratyush/headphonequicktile/ui/ConfigActivity.kt
+++ app/src/main/java/dev/pratyush/headphonequicktile/ui/ConfigActivity.kt
@@ -1,27 +1,42 @@
 package dev.pratyush.headphonequicktile.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.updatePadding
+import androidx.core.app.ActivityCompat
+import androidx.core.view.WindowCompat
 import dev.pratyush.headphonequicktile.databinding.ConfigActivityBinding
 
 class ConfigActivity : AppCompatActivity() {
 
-    private lateinit var views : ConfigActivityBinding
+    private val permission = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        arrayOf(
+            Manifest.permission.BLUETOOTH_CONNECT,
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_ADMIN,
+        )
+    } else {
+        arrayOf(
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_ADMIN,
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setDecorFitsSystemWindows(true)
+        WindowCompat.setDecorFitsSystemWindows(window, true)
 
-        views = ConfigActivityBinding.inflate(layoutInflater)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.BLUETOOTH_CONNECT
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(permission, 10)
+        }
+
+        val views = ConfigActivityBinding.inflate(layoutInflater)
         setContentView(views.root)
-
-       /* supportFragmentManager.beginTransaction().add(
-            views.fragmentContainer.id,
-            SettingsPreference()
-        ).commit()*/
-
     }
 }
