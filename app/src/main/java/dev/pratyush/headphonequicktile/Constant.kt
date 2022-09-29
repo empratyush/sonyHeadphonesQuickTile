428d6bd79554fd3a7a1f78abc4a3245b3c4e21c7
Pratyush <codelab@pratyush.dev>
2022-09-29 18:29:20 +0530

    added support for wf1000xm4

--- app/src/main/java/dev/pratyush/headphonequicktile/Constant.kt
+++ app/src/main/java/dev/pratyush/headphonequicktile/Constant.kt
@@ -5,3 +5,13 @@ import java.util.UUID
 val uuid: UUID = UUID.fromString("96CC203E-5068-46ad-B32D-E316F5E069BA")
 val uuidAlt: UUID = UUID.fromString("BA69E0F5-16E3-2DB3-ad46-68503E20CC96")
 val powerOff = byteArrayOf(62, 12, 0, 0, 0, 0, 3, 34, 0, 1, 50, 60)
+
+val uuid_wf1000xm4 : UUID = UUID.fromString("956c7b26-d49a-4ba8-b03f-b17d393cb6e2")
+val powerOff_wf1000xm4 =byteArrayOf(62, 12, 1, 0, 0, 0, 3, 36, 3, 1, 56, 60)
+/*byteArrayOf(62, 12,
+    1, //speak to chat `enabled ? 1 : 0`
+    0, 0, 0, 3, 36, 3, 1,
+    56, //noise canceling
+    60
+)*/
+
