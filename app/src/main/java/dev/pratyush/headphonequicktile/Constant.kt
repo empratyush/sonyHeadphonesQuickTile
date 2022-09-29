8de16888b7c95d6d79857eeffc06b2a57d39f592
Pratyush <codelab@pratyush.dev>
2022-09-29 13:16:09 +0530

    remove global import

--- app/src/main/java/dev/pratyush/headphonequicktile/Constant.kt
+++ app/src/main/java/dev/pratyush/headphonequicktile/Constant.kt
@@ -1,7 +1,7 @@
 package dev.pratyush.headphonequicktile
 
-import java.util.*
+import java.util.UUID
 
-val uuid: UUID = UUID.fromString("96cc203e-5068-46ad-b32d-e316f5e069ba")
-val uuidAlt: UUID = UUID.fromString("ba69e0f5-16e3-2db3-ad46-68503e20cc96")
+val uuid: UUID = UUID.fromString("96CC203E-5068-46ad-B32D-E316F5E069BA")
+val uuidAlt: UUID = UUID.fromString("BA69E0F5-16E3-2DB3-ad46-68503E20CC96")
 val powerOff = byteArrayOf(62, 12, 0, 0, 0, 0, 3, 34, 0, 1, 50, 60)
